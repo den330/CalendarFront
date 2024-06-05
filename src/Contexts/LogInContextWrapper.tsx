cb510fa728791767813fe777786beb29b94bf3e3
den330 <yaxinyuan0910@gmail.com>
2024-06-05 13:51:12 -0400

    app

--- src/Contexts/LogInContextWrapper.tsx
+++ src/Contexts/LogInContextWrapper.tsx
@@ -1,16 +1,16 @@
 import { ReactNode, createContext, useState } from "react";
 
 type LogInContextType = {
-  loggedIn: boolean;
+  isLoggedIn: boolean;
   setLoggedIn: (loggedIn: boolean) => void;
 };
 
 export const LogInContext = createContext<null | LogInContextType>(null);
 
 export const LogInContextWrapper = ({ children }: { children: ReactNode }) => {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
-    <LogInContext.Provider value={{ loggedIn, setLoggedIn }}>
+    <LogInContext.Provider value={{ isLoggedIn, setLoggedIn }}>
       {children}
     </LogInContext.Provider>
   );
