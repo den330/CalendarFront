1c0c13600f9dcfc6f8378c7cc5cd7fc00d3cd786
den330 <wo131485508@qq.com>
2024-06-06 00:20:36 -0400

    form

---
+++ src/Components/AuthForm.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import SignCategory from "../Types/SignCategory";
+import { signup, login } from "../Utilities/ConnectionHub";
+
+export default function AuthForm({ category }: { category: SignCategory }) {
+  interface Credential {
+    email: string;
+    password: string;
+  }
+  const [credential, setCredential] = useState<Credential>({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setCredential((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      if (category === SignCategory.SignUp) {
+        await signup(credential.email, credential.password);
+      } else {
+        login(credential.email, credential.password);
+      }
+    } catch (error) {
+      alert(`${category} failed: ${error}`);
+    }
+  }
+  return (
+    <div className="flex flex-col">
+      <h1>{category}</h1>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" onChange={handleChange} />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={handleChange}
+        />
+        <button type="submit">{category}</button>
+      </form>
+    </div>
+  );
+}
